c971c40781ecb11a6edbe280ed2405eb94d3295c
administrator <administrator@administrators-imac.local>
2019-03-07 16:42:03 -0500

    updated add item button

--- Todooey2/TodoListViewController.swift
+++ Todooey2/TodoListViewController.swift
@@ -45,5 +45,29 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    
+    @IBAction func addButttonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "",
+                                      preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+                //what will happen once user clicks UIAlert
+            print(textField.text)
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+             
+            
+        }
+            
+            alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
 }
 
