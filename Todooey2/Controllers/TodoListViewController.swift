33526ab99d52cbedeb83a8a4e96aed820e221170
Paul Richardson <paulrichardson@pauls-imac.local>
2019-03-10 16:55:11 -0400

    pupated with model framework and MVC

--- Todooey2/Controllers/TodoListViewController.swift
+++ Todooey2/Controllers/TodoListViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class TodoListViewController: UITableViewController {
 
     
-        var itemArray=["Find Mike", "Buy Eggs", "Destroy Dragon"]
+        var itemArray=[Item]()
     
         let defaults = UserDefaults.standard
     
@@ -19,10 +19,23 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let  items = defaults.array(forKey: "TodoListArray") as? [String] {
+         let newItem = Item()
+         newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Compare Blueberry Prices"
+        itemArray.append(newItem3)
+        
+        
+        
+        
+         if let  items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
-            
-            
         }
         
     }
@@ -33,7 +46,14 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-            cell.textLabel?.text = itemArray[indexPath.row]
+            let item = itemArray[indexPath.row]
+            cell.textLabel?.text = item.title
+        
+        //    cell.textLabel?.text = itemArray[indexPath.row].title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        
         
             return cell
         
@@ -42,15 +62,12 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // print(itemArray[indexPath.row])
         
-        if
-            tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
         
+        
+        
+        tableView.reloadData()
     
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -65,7 +82,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
                 //what will happen once user clicks UIAlert
             
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            
+            self.itemArray.append(newItem)
+            
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Todooey2/Data Model/item.swift
@@ -0,0 +1,15 @@
+//
+//  item.swift
+//  Todooey2
+//
+//  Created by Paul Richardson on 3/10/19.
+//  Copyright © 2019 Paul Richardson. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
